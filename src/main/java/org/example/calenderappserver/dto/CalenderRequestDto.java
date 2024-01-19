fae3491721470b4d6e530c69a11c851407b232a2
SerenityZenDev <tjrqo2000@gmail.com>
2024-01-19 17:56:08 +0900

    :sparkles: Dto getter and constructor 생성

--- src/main/java/org/example/calenderappserver/dto/CalenderRequestDto.java
+++ src/main/java/org/example/calenderappserver/dto/CalenderRequestDto.java
@@ -1,10 +1,8 @@
 package org.example.calenderappserver.dto;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import lombok.Getter;
 
+@Getter
 public class CalenderRequestDto {
     private String title;
     private String context;
--- src/main/java/org/example/calenderappserver/dto/CalenderResponseDto.java
+++ src/main/java/org/example/calenderappserver/dto/CalenderResponseDto.java
@@ -1,10 +1,7 @@
 package org.example.calenderappserver.dto;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import java.time.LocalDateTime;
+import org.example.calenderappserver.entity.Calender;
 
 public class CalenderResponseDto {
     private Long scheduleId;
@@ -12,4 +9,12 @@ public class CalenderResponseDto {
     private String context;
     private String userName;
     private LocalDateTime createdAt;
+
+    public CalenderResponseDto(Calender calender) {
+        this.scheduleId = calender.getScheduleId();
+        this.title = calender.getTitle();
+        this.context = calender.getContext();
+        this.userName = calender.getUserName();
+        this.createdAt = calender.getCreatedAt();
+    }
 }
