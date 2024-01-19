fb39088cb7049460f3d65ec89f39eb5938cbfe2a
SerenityZenDev <tjrqo2000@gmail.com>
2024-01-19 15:33:15 +0900

    :sparkles: Entity field setting

--- src/main/java/org/example/calenderappserver/entity/Calender.java
+++ src/main/java/org/example/calenderappserver/entity/Calender.java
@@ -1,5 +1,23 @@
 package org.example.calenderappserver.entity;
 
-public class Calender {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Calender extends Timestamped {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long scheduleId;
+    @Column
+    private String title;
+    @Column
+    private String context;
+    @Column
+    private String userName;
+    @Column
+    private String password;
 
 }
