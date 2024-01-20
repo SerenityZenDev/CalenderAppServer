d1f59c89155ddf185163154156c987779818b323
SerenityZenDev <tjrqo2000@gmail.com>
2024-01-20 11:08:28 +0900

    :sparkles: DB 연동을 위한 세팅

--- src/main/java/org/example/calenderappserver/entity/Calender.java
+++ src/main/java/org/example/calenderappserver/entity/Calender.java
@@ -5,8 +5,17 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.example.calenderappserver.dto.CalenderRequestDto;
 
 @Entity
+@Getter
+@Setter
+@Table(name = "calender")
+@NoArgsConstructor
 public class Calender extends Timestamped {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,4 +29,10 @@ public class Calender extends Timestamped {
     @Column
     private String password;
 
+    public Calender(CalenderRequestDto calenderRequestDto) {
+         this.title = calenderRequestDto.getTitle();
+         this.context = calenderRequestDto.getContext();
+         this.userName = calenderRequestDto.getUserName();
+         this.password = calenderRequestDto.getPassword();
+    }
 }
