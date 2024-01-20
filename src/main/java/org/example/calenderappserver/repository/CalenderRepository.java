5ee17824322d24079b43bd87f38f1ad70b4a4601
SerenityZenDev <tjrqo2000@gmail.com>
2024-01-20 11:07:11 +0900

    :sparkles: 전체 조회 작성일 기준 내림차순 JPA 구현

--- src/main/java/org/example/calenderappserver/repository/CalenderRepository.java
+++ src/main/java/org/example/calenderappserver/repository/CalenderRepository.java
@@ -1,7 +1,11 @@
 package org.example.calenderappserver.repository;
 
+import java.util.List;
+import org.example.calenderappserver.entity.Calender;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface CalenderRepository extends JpaRepository {
-
+@Repository
+public interface CalenderRepository extends JpaRepository<Calender, Long> {
+    List<Calender> findAllByOrderByCreatedAtDesc();
 }
