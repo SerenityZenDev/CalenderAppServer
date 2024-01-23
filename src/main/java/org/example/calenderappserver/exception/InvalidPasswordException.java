2347ea2fdea91bb366a9817c9fe0f968438a121c
SerenityZenDev <tjrqo2000@gmail.com>
2024-01-23 11:54:48 +0900

    :recycle: Exception 방식 변경

--- src/main/java/org/example/calenderappserver/exception/InvalidPasswordException.java
+++
@@ -1,8 +0,0 @@
-package org.example.calenderappserver.exception;
-
-public class InvalidPasswordException extends RuntimeException {
-
-    public InvalidPasswordException(String message) {
-        super(message);
-    }
-}
